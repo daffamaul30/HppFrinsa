ee8688384001853eadb3c79ce5fd7fb944a33161
Daffa <daffamaulana30@gmail.com>
2020-06-08 13:19:27 +0800

    Splash Screen

---
+++ app/src/main/java/frinsa/hpp/SplashScreen.kt
@@ -0,0 +1,22 @@
+package frinsa.hpp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var handler: Handler
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this@SplashScreen, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },5000)
+    }
+}
