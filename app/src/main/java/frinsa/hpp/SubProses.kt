e8717feeaba91d96a89854a7970ce05116e532ba
Daffa <daffamaulana30@gmail.com>
2020-07-04 18:41:54 +0800

    update

--- app/src/main/java/frinsa/hpp/SubProses.kt
+++ app/src/main/java/frinsa/hpp/SubProses.kt
@@ -1,5 +1,6 @@
 package frinsa.hpp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import frinsa.hpp.daftar_produksi.ModelDaftarProduksi
 import frinsa.hpp.data.DBPanen
 import frinsa.hpp.data.Produk
+import frinsa.hpp.tahapan_proses.TahapanProses
 import kotlinx.android.synthetic.main.activity_sub_proses.*
 import java.util.*
 import kotlin.collections.ArrayList
@@ -62,7 +64,7 @@ class SubProses: AppCompatActivity(), View.OnClickListener {
 //        val adapterproses = SubProsesAdapter (this, displayproses)
 //    }
 
-    private fun validation(): Boolean {
+    private fun validation(): Pair<Boolean, String?> {
         var valid = true
         if (posisi.size > 1) {
             for (i in 0 until posisi.size) {
@@ -74,8 +76,10 @@ class SubProses: AppCompatActivity(), View.OnClickListener {
                     }
                 }
             }
+            //MERGE DATANYA DI DATABASE
         }
-        return valid
+
+        return Pair(valid,spList.get(posisi.get(0)).proses)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -123,20 +127,30 @@ class SubProses: AppCompatActivity(), View.OnClickListener {
         return true
     }
 
+    fun getCode(): String {
+        val list = it.split(".")
+    }
+
+    fun String.capitalizeWords(): String = split(" ").map { it.capitalize() }.joinToString(" ")
+
     override fun onClick(v: View) {
         when (v.id) {
             R.id.btn_proses -> {
-                val valid = validation()
+                val (valid,name) = validation()
                 Toast.makeText(this, valid.toString(), Toast.LENGTH_SHORT).show()
 
                 if (valid) {
-                    var stringBuilder = StringBuilder()
-                    posisi.forEach {
-                        stringBuilder.append(spList.get(it).proses).append(" ").append(spList.get(it).tahap).append("\n")
-                    }
-//                println(posisi.size)
-                    println(stringBuilder)
-                    Toast.makeText(this, stringBuilder, Toast.LENGTH_SHORT).show()
+                    val step = name.toString()?.let { db.getStepProses(it) }
+                    val kode =
+//                    var stringBuilder = StringBuilder()
+//                    posisi.forEach {
+//                        stringBuilder.append(spList.get(it).proses).append(" ").append(spList.get(it).tahap).append("\n")
+//                    }
+//                    println(stringBuilder)
+//                    Toast.makeText(this, stringBuilder, Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this@SubProses, TahapanProses::class.java)
+                    intent.putExtra(TahapanProses.KODE_FRAG, kode)
+                    intent.putExtra(TahapanProses.TITLE, kode.capitalizeWords())
                 }
             }
         }
