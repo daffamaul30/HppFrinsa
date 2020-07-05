4d02b6f691a6d5fd03262c841b34c71b28e9d0f7
Daffa <daffamaulana30@gmail.com>
2020-07-05 10:34:12 +0800

    update

--- app/src/main/java/frinsa/hpp/SubProses.kt
+++ app/src/main/java/frinsa/hpp/SubProses.kt
@@ -80,7 +80,7 @@ class SubProses: AppCompatActivity(), View.OnClickListener {
                 if (i == posisi.size-1) {
                     break
                 } else {
-                    if ((spList.get(posisi.get(i)).proses == spList.get(posisi.get(i+1)).proses) || (spList.get(posisi.get(i)).tahap == spList.get(posisi.get(i+1)).tahap)) {
+                    if ((spList.get(posisi.get(i)).proses != spList.get(posisi.get(i+1)).proses) || (spList.get(posisi.get(i)).tahap != spList.get(posisi.get(i+1)).tahap)) {
                         valid = false
                     }
                 }
